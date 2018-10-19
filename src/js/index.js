3a35171c3bbc74bcbc87c71430589ad7f27d588d
Samanta <slaragiannone@gmail.com>
2018-10-19 17:16:44 +0100

    cambios hechos en clase

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,34 @@
-const equivalencia = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I"
+const equivalencia = { 1000: "M",
+900: "CM",
+500: "D",
+400: "CD",
+100: "C",
+90: "XC",
+50: "L",
+40: "XL",
+10: "X",
+9: "IX",
+5: "V",
+4: "IV",
+1: "I"
 };
-const equivalencia_keys = Object.keys(equivalencia).reverse();
-//al poner .key devuelve un array con las propiedades del objeto equivalencia
+    
+const equiv = Object.keys(equivalencia).reverse();
+console.log(equiv);
+//al poner .keys devuelve un array con las propiedades del objeto equivalencia
 export const num = (value) => {
 
-    if (typeof num !== 'number' || num < 0 || !Number.isInteger(num)) {
-        console.log('La entrada no es válida');
+    if (typeof num !== 'number' || num <= 0 || !Number.isInteger(num)) {
+        throw new Error('No number'); //de esta manera se pone un error
     }
 
     
     let romano = [];
 
-    equivalencia_keys.forEach(key => {
+    equiv.forEach(key => {
         while (num >= key) {
-            num = num -key
-            romano = romano+ equivalencia[key]
+            num = num - key;
+            romano = romano + equiv[key];
         }
 });
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,12 @@
 import { num } from "../src/js";
 
-describe('num', () => {
-    test('Debería decir que no es una entrada válida', () => {
-        expect(num('hola')).toBe('I');
-    });
-    test('Debería convertir número a romano - Letra I', () => {
-        expect(num(1)).toBe('I');
+describe('Index', () => {  
+    describe('num', () => {
+        test('Debería decir que no es una entrada válida', () => {
+            expect(() => num('No number')).toThrow();
+        });
+        test('Debería convertir número a romano - Letra I', () => {
+            expect(num(1)).toBe('I');
+        });
     });
 });
